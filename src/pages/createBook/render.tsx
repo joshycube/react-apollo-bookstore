78305e3570f7d64dea38e4968e94280781dbd062
Jozsef M Komjati <jozsef.komjati@blackcatsolutions.co.uk>
2019-07-18 19:31:20 +0100

    Import skeleton forms to pages

--- src/pages/createBook/render.tsx
+++ src/pages/createBook/render.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+import CreateBookForm from "@/components/forms/createBook";
 import { SceneWrapper } from "../style";
 
-const CreateBookPage = () => <SceneWrapper />;
+const CreateBookPage = () => (
+  <SceneWrapper>
+    <CreateBookForm />
+  </SceneWrapper>
+);
 
 export default CreateBookPage;
--- src/pages/editBook/render.tsx
+++ src/pages/editBook/render.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+import EditBookForm from "@/components/forms/editBook";
 import { SceneWrapper } from "../style";
 
-const EditBookPage = () => <SceneWrapper />;
+const EditBookPage = () => (
+  <SceneWrapper>
+    <EditBookForm />
+  </SceneWrapper>
+);
 
 export default EditBookPage;
