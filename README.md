a7950765e5d621ae57be6e5af37e2bef9497b701
Jozsef M Komjati <jozsef.komjati@blackcatsolutions.co.uk>
2019-07-18 22:16:14 +0100

    Add Readme and clean up

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # react-apollo-bookstore
+
 React Apollo Demonstration
+
+# to run the project
+
+Simply do `yarn` then `yarn start`
+and the project will start on `http://localhost:3000`
+
+# improvements / todos
+
+- UI DESIGN!!!
+- Add tests to achieve a good coverage
+- Move the settings to .env
+- Add Intl and JSON objects for strings and tokens to make them translatable
+- Add a ThemeProvider for styled-components, create a theme so that colors, fonts, sizing, spacing can be moved there
+- Add a proper loader - suspense + a loader component?
+- Add proper error handling
+- Add 404 and other error pages
+
+# buglist
+
+- Adding a book to the basket then editing and changing its price won't update the basket total
+- Check the fetchPolicy - is network-only needed for the edit form?
--- src/utils/getApolloClient.ts
+++ src/utils/getApolloClient.ts
@@ -1,24 +1,22 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import apolloSettings from '@/apollo';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import apolloSettings from "@/apollo";
 
-import Link from './createApolloLink';
+import Link from "./createApolloLink";
 
 let client: any = null;
 
-/* istanbul ignore else */
 if (!client) {
   const cache = new InMemoryCache();
   client = new ApolloClient({
     link: Link,
     cache,
     resolvers: apolloSettings.mutations,
-    typeDefs: apolloSettings.typeDefs,
+    typeDefs: apolloSettings.typeDefs
   });
 
-  // Prep the cache with our defaults
   cache.writeData({
-    data: apolloSettings.defaults,
+    data: apolloSettings.defaults
   });
 }
 
