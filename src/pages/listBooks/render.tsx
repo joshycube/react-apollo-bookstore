da3d16a3ae989198f0b921b9bc26450775801801
Jozsef M Komjati <jozsef.komjati@blackcatsolutions.co.uk>
2019-07-18 18:45:02 +0100

    Add navigation to page

--- src/pages/listBooks/render.tsx
+++ src/pages/listBooks/render.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Books from "@/components/books";
+import Navigation from "@/components/navigation";
 import { SceneWrapper } from "../style";
 
 const ListBooksPage = () => (
   <SceneWrapper>
+    <Navigation />
     <Books />
   </SceneWrapper>
 );
